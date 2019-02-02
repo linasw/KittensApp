b2337287b067411d02d353124e881a95a36af077
linasw <linasw96@gmail.com>
2019-02-02 20:50:30 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {NativeModules} from 'react-native';
+
+import App from './App.container';
+
+class Kittens extends Component {
+  render () {
+    return (
+      <App />
+    );
+  }
+}
+
+export default Kittens;
